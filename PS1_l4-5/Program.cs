435a27f09edb66aceff2a29365c9d5fd4e1cb4c4
Nerond32 <nerond32@gmail.com>
2017-05-03 19:29:39 +0200

    l2-l4

---
+++ PS1_l4-5/Program.cs
@@ -0,0 +1,89 @@
+﻿using PS1_l4_5;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace PS1_l4_5
+{
+    class Program
+    {
+        private static readonly int taskAmount = 10;
+        private static List<AlphabetWriter> alphabetWriters;
+        static void Main(string[] args)
+        {
+            alphabetWriters = new List<AlphabetWriter>();
+            for (int i = 0; i < taskAmount; i++)
+                alphabetWriters.Add(new AlphabetWriter(i));
+            alphabetWriters.ForEach(x => Task.Run(() => x.Write()));
+            InterpretCommand();
+        }
+
+        static void InterpretCommand()
+        {
+            string userInput = null;
+            do
+            {
+                userInput = Console.ReadLine().ToLower();
+                try
+                {
+                    InterpretCommand(userInput);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Invalid command");
+                }
+            } while (userInput != "end");
+        }
+        static void InterpretCommand(String command)
+        {
+            string[] splittedCommand = command.Split(' ');
+            List<int> thrNumber = RecognizeNumbers(splittedCommand[1]);
+            if (splittedCommand[0] == "abort")
+            {
+                if (thrNumber.Count == 1)
+                {
+                    AbortTask(thrNumber[0]-2);
+                }
+                else if (thrNumber.Count == 2)
+                {
+                    for (int i = thrNumber[0]; i <= thrNumber[1]; i++)
+                    {
+                        AbortTask(i-2);
+                    }
+                }
+            }
+            else if (splittedCommand[0] != "end")
+            {
+                Console.WriteLine("Invalid command");
+            }
+        }
+        static void AbortTask(int number)
+        {
+            var target = alphabetWriters.FirstOrDefault(x => x.Number == number);
+            if (target == null)
+                Console.WriteLine("Unable to find task with number {1}", number);
+            else
+            {
+                target.Cancel();
+            }
+        }
+        static List<int> RecognizeNumbers(String numbers)
+        {
+            List<int> intList = new List<int>();
+            int x;
+            if (int.TryParse(numbers, out x))
+            {
+                intList.Add(x);
+            }
+            else
+            {
+                String[] s = numbers.Split('-');
+                intList.Add(Int32.Parse(s[0]));
+                intList.Add(Int32.Parse(s[1]));
+            }
+            return intList;
+        }
+    }
+}
---
+++ PS1_l4-6/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PS1_l4_6
+{
+    class Program
+    {
+        private static readonly int philosopherAmount = 5;
+        private static bool[] forkIsUsed = new bool[philosopherAmount];
+        private static Thread[] philosopherThread = new Thread[philosopherAmount];
+        private static object o = new object();
+        //This delegate can be used to point to methods
+        //which return void and take a string.
+        public delegate void MyEventHandler();
+
+        //This event can cause any method which conforms
+        //to MyEventHandler to be called.
+        public event MyEventHandler SomethingHappened;
+        void HandleSomethingHappened()
+        {
+            //Do some stuff
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < philosopherAmount; i++)
+            {
+                int z = i;
+                forkIsUsed[z] = false;
+                philosopherThread[i] = new Thread(() => Philosophing(z));
+                philosopherThread[i].Start();
+            }
+        }
+        static void Philosophing(int fork1Number)
+        {
+            int fork2Number = fork1Number + 1;
+            if (fork2Number == philosopherAmount)
+            {
+                fork2Number = 0;
+            }
+            while (true)
+            {
+                bool nSHouldWait;
+                lock (o)
+                {
+                    if(forkIsUsed[fork1Number] == false && forkIsUsed[fork2Number])
+                    {
+                        forkIsUsed[fork1Number] = true;
+                        forkIsUsed[fork2Number] = true;
+                        nSHouldWait = false;
+                    }
+                    else
+                    {
+                        nSHouldWait = true;
+                    }
+                }
+                if (nSHouldWait)
+                {
+                    //WaitForSingleObject(hndEvents[1], INFINITE);
+                }
+            }
+        }
+    }
+}
